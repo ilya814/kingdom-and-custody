285f1c7490ce0222387b71907faa1b8399d365bf
ilya814 <ilyatascken@gmail.com>
2026-03-22 13:30:16 +0100

    Add src/main/java/com/ilya814/kingdomandcustody/client/KingdomAndCustodyClient.java

---
+++ src/main/java/com/ilya814/kingdomandcustody/client/KingdomAndCustodyClient.java
@@ -0,0 +1,10 @@
+package com.ilya814.kingdomandcustody.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KingdomAndCustodyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-side registration (armor renderer layers etc.) goes here
+    }
+}
